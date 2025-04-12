bed90c9c0f7ad01becde241f7b64739eeb5bdea2
user.name "Eisa Khan <user.email "khanarafat1376@gmail.com>
2025-04-13 04:44:36 +0530

    feat: update route components to include Navbar and Footer

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <Navbar /> 
+      <Footer />
+    </div>
   )
 }
 

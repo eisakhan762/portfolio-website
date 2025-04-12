e039a5c0161ff94688e744f93bd2726e0b379639
user.name "Eisa Khan <user.email "khanarafat1376@gmail.com>
2025-04-13 03:36:49 +0530

    feat: implement dynamic background color change on scroll in Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,9 +6,18 @@ import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
+  const [color, setColor] = useState(false);
   const handleCLick = () => setClick(!click);
+  const changeColor = () => {
+    if (window.scrollY >= 100) {
+      setColor(true)
+    }else {
+      setColor(false)
+    }
+  }
+  window.addEventListener("scroll", changeColor)
   return (
-    <div className="header">
+    <div className={color ? "header header-bg" : "header"}>
       <Link to="/">
         <h1>Portfolio.</h1>
       </Link>
